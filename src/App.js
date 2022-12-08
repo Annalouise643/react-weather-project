d6c12003dab30413a9afd01dc2a583c510303670
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-12-08 07:49:33 +0000

    Updated React scripts and added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello World</h1>
+          <button className="btn btn-primary">Hello</button>
+        </header>
+      </div>
     </div>
   );
 }
