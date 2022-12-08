c70e86ddca5d194d33bd3a6004125b92bda41366
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-12-08 08:19:20 +0000

    Added basic HTML

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Hello World</h1>
-          <button className="btn btn-primary">Hello</button>
-        </header>
+        <h1>Weather App</h1>
+        <Weather />
       </div>
     </div>
   );
