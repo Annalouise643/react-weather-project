979ffd4bb36fd8e4eea61e521db80f61f77c2700
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-12-09 08:43:33 +0000

    Cleaned up files - React Weather Search Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
@@ -11,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
