5fe06b38245049979bf25266aaab10c5a9e4baed
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-12-09 08:55:59 +0000

    Added repo url - Lesson 2 React Weather Search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ export default function App() {
       <div className="container">
         <h1>Weather App</h1>
         <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://infallible-goldberg-4e9ba6.netlify.app/"
+            target="_blank"
+          >
+            Anna Bray
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/Annalouise643/react-weather-project"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
